fa08e61ec8111197395e1e8d59e4b5d0aa5a2d71
Shamim <shamim@Shamims-MacBook-Pro.local>
2020-09-14 20:11:22 +0600

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC3CogRMHvh5vh5FA6cZpEtzx447JjyfcA",
+    authDomain: "burj-al-arab-e5059.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-e5059.firebaseio.com",
+    projectId: "burj-al-arab-e5059",
+    storageBucket: "burj-al-arab-e5059.appspot.com",
+    messagingSenderId: "785958039407",
+    appId: "1:785958039407:web:be1fdff888eeb8f177029a"
+  };
+
+  export default firebaseConfig
